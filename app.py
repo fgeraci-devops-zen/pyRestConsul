e6fe7fb8438542600e5df5d9ee43484daf768d1d
fgeraci-devops-zen <fgeraci@devops-zen.com>
2017-08-06 18:28:59 +0200

    Typo Fixed

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return "Ok, ci sono. Meet me @ port #5000"
-
-
-
     c = consul.Consul(host='127.0.0.1')
 
     # Register Service
     c.agent.service.register('pyRestNoSwarm',
                              service_id='pyRestNoSwarm',
-                             port=5000)
+                             port=5000,
+                             tags=[])
+    print(c.agent.services())
+    # List all registered Services
+    for x in c.agent.services():
+        print(x)
+        # To remove the service entry
+        # c.agent.service.deregister(service_id='my_http_service_1')
+    return "Ok, ci sono. Meet me @ port #5000"
 
+home()
 app.run(host='0.0.0.0', port=5000)
